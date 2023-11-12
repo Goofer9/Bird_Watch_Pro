ba266ec015785ef8dcd17c2fdbe023c5ad07dd40
ST10119248 <108737558+Goofer9@users.noreply.github.com>
2023-11-12 12:54:05 +0200

    Blessed

---
+++ app/src/main/java/com/example/birdtrackingapp/Observations.kt
@@ -0,0 +1,11 @@
+package com.example.birdtrackingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Observations : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_observations)
+    }
+}
---
+++ app/src/main/java/com/example/birdtrackingapp/collection.kt
@@ -0,0 +1,11 @@
+package com.example.birdtrackingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class collection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_collection)
+    }
+}
