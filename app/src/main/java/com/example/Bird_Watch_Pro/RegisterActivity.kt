e8240ec94dd117bac16b5cc1183eea21cd3f2ba5
ST10119248 <108737558+Goofer9@users.noreply.github.com>
2023-11-13 14:00:10 +0200

    Blessed

--- app/src/main/java/com/example/Bird_Watch_Pro/RegisterActivity.kt
+++ app/src/main/java/com/example/Bird_Watch_Pro/RegisterActivity.kt
@@ -1,11 +1,34 @@
 package com.example.Bird_Watch_Pro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        // Initialize your views
+        val tvRegisterTitle = findViewById<TextView>(R.id.tv_register_title)
+        val tvRegisterSubTitle = findViewById<TextView>(R.id.tv_register_sub_title)
+        val tilFullName = findViewById<TextInputLayout>(R.id.til_fullname)
+        val etFullName = findViewById<TextInputEditText>(R.id.et_fullname)
+        val tilEmail = findViewById<TextInputLayout>(R.id.til_email)
+        val etEmail = findViewById<TextInputEditText>(R.id.et_email)
+        val tilUsername = findViewById<TextInputLayout>(R.id.til_username)
+        val etUsername = findViewById<TextInputEditText>(R.id.et_username)
+        val btnRegister = findViewById<AppCompatButton>(R.id.btn_register)
+        val tvHaveAccount = findViewById<TextView>(R.id.tv_have_account)
+
+        // Add any additional setup or logic you need here
+
+        // Example: Setting a click listener for the register button
+        btnRegister.setOnClickListener {
+            // Perform registration logic here
+        }
     }
 }
