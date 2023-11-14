13947b7ace0d3a9f3cae10ed976fe8a82bc2571e
ST10119248 <108737558+Goofer9@users.noreply.github.com>
2023-11-14 16:57:46 +0200

    Blessed

--- app/src/main/java/com/example/Bird_Watch_Pro/MainActivity.kt
+++ app/src/main/java/com/example/Bird_Watch_Pro/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.Bird_Watch_Pro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_login.setOnClickListener {
+            // Handle login button click
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        btn_register.setOnClickListener {
+            // Handle register button click
+            Toast.makeText(this, "Register button clicked", Toast.LENGTH_SHORT).show()
+            // You can navigate to the registration screen or perform other actions
+        }
     }
 }
--- app/src/main/java/com/example/Bird_Watch_Pro/collection.kt
+++ app/src/main/java/com/example/Bird_Watch_Pro/collection.kt
@@ -1,11 +1,31 @@
 package com.example.Bird_Watch_Pro
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.Bird_Watch_Pro.R.*
 
-class collection : AppCompatActivity() {
+class Collection : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_collection)
+        setContentView(layout.activity_main)
+
+        val etFullness = findViewById<EditText>(id.et_fullname)
+        val btnEditCollection = findViewById<Button>(id.btn_edit_collection)
+        val btnViewOrCompareSpecies = findViewById<Button>(id.btn_view_species)
+
+        btnEditCollection.setOnClickListener {
+            val fullName = etFullness.text.toString()
+            // Handle the "EDIT COLLECTION" button click
+            Toast.makeText(this, "Editing collection for $fullName", Toast.LENGTH_SHORT).show()
+        }
+
+        btnViewOrCompareSpecies.setOnClickListener {
+            // Handle the "VIEW OR COMPARE SPECIES" button click
+            Toast.makeText(this, "Viewing or comparing species", Toast.LENGTH_SHORT).show()
+        }
     }
 }
